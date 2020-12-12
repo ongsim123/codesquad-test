4645d4c7c02fc8c0da8ff15710164c428ac2854d
박정현 <>
2020-12-12 13:05:23 +0900

    Correct a typo in alert

--- cube1.js
+++ cube1.js
@@ -8,7 +8,7 @@ function main() {
     let absNum = Math.abs(num); // 사용자 입력 숫자 양수 음수 판별
 
     if (num < -100 || num >= 100) { // 사용자 입력 숫자 범위 초과 시 알람 발생
-        alert(`정수값이 -100이상 100미만인지 확인하시오.<br>올바른 방향을 입력했는지 확인하시오.`);
+        alert(`정수값이 -100이상 100미만인지 , 올바른 방향을 입력했는지 확인 해주세요.`);
     }
 
     if ((direction === 'L' && num == absNum) || (direction === 'R' && num != absNum)) { // 방향이 L이고 양수이거나, 방향이 R이고 음수일 경우
